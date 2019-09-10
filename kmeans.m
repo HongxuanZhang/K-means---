6634e722c273031f9f2bc3773b91e024146de28b
HongxuanZhang <1173071213@qq.com>
2019-09-10 17:38:21 +0800

    now result can give out

--- kmeans.m
+++ kmeans.m
@@ -10,20 +10,21 @@ cluster_center = data(First_id,:);
 
 cluster_center_new = zeros(size(cluster_center));
 first = 1;
+
+cluster_index = [];
+cluster_num = [];
+
 while ~all(cluster_center_new == cluster_center) 
     if ~first
         cluster_center = cluster_center_new;
     else
         first = 0;
     end
-    cluster_index = zeros(k,sample_size); % 每一行代表某个聚类中所有的数据索引值
     
+    cluster_index = zeros(k,sample_size); % 每一行代表某个聚类中所有的数据索引
     cluster_num = ones(k,1);             % 表示cluster_index矩阵存储到哪了(已存的)
     
     for i = 1:sample_size
-%         if any(i == cluster_center)
-%             continue;
-%         end
         Euclidean_distance = pdist2(data(i,:),cluster_center,'euclidean'); %计算欧式距离
         min_dist_index = (Euclidean_distance==min(Euclidean_distance));
         % 为防止有多个最小值，选取第一个作为距离最近的聚类中心
@@ -31,9 +32,16 @@ while ~all(cluster_center_new == cluster_center)
         cluster_index(I1,cluster_num(I1)) = i;
         cluster_num(I1) = cluster_num(I1)+1;
     end
-    
     % 现在已经得到了每个样本的模式类，根据这些模式类，计算新的聚类中心
     cluster_center_new = FindNewCenter(cluster_index,cluster_num,data);
-    
 end
-cluster_center_new
+
+for i2 = 1:k
+    fprintf('第%d个聚类中心为',i2);
+    cluster_center_new(i2,:)
+    fprintf('包含样本有\n');
+    for j1 = 1:cluster_num(i2)-1
+        fprintf('%d ',cluster_index(i2,j1));
+    end
+    fprintf('\n');
+end
