748bf20d6fadf5bffea1b7add97ca72ca044af7a
HongxuanZhang <1173071213@qq.com>
2019-09-10 13:00:20 +0800

    计算所有的样本点分类

--- kmeans.m
+++ kmeans.m
@@ -11,10 +11,22 @@ cluster_center = data(First_id,:);
 cluster_center_new = zeros(size(cluster_center));
 
 while all(cluster_center_new == cluster_center) 
+    cluster_index = zeros(k,sample_size); % 每一行代表某个聚类中所有的数据索引值
+    
+    cluster_num = ones(k,1);             % 表示cluster_index矩阵存储到哪了(已存的)
+    
     for i = 1:sample_size
 %         if any(i == cluster_center)
 %             continue;
 %         end
         Euclidean_distance = pdist2(data(i),cluster_center,'euclidean'); %计算欧式距离
+        min_dist_index = (Euclidean_distance==min(Euclidean_distance));
+        % 为防止有多个最小值，选取第一个作为距离最近的聚类中心
+        I1= find(min_dist_index,1,'first');
+        cluster_index(I1,cluster_num(I1)) = i;
+        cluster_num(I1) = cluster_num(I1)+1;
     end
+    
+    % 现在已经得到了每个样本的模式类，根据这些模式类，计算新的聚类中心
+    
 end
